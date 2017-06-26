7c7444301320a94e91d8e2ccd288e78dff7f6831
Anshula Singh <anshula.singh@gmail.com>
2017-06-26 14:55:55 -0700

    Initial commit

---
+++ build/Pods.build/Debug-iphoneos/Pods-Makestagram.build/DerivedSources/Pods_Makestagram_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_MakestagramVersionString[];
+ extern const double Pods_MakestagramVersionNumber;
+
+ const unsigned char Pods_MakestagramVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_Makestagram  PROJECT:Pods-1" "\n";
+ const double Pods_MakestagramVersionNumber __attribute__ ((used)) = (double)1.;
