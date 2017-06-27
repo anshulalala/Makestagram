3d345c80ddeaef7d3e243611beba2f2ee2d21331
Anshula Singh <anshula.singh@gmail.com>
2017-06-27 10:49:00 -0700

    1st commit up to step 6

---
+++ LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  Makestagram
+//
+//  Created by Anurag on 6/27/17.
+//  Copyright © 2017 Anshula. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
